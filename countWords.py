817d98201008ef061f5aabac3ffcdb4b308c6065
luisren64 <lrenteria5@miners.utep.edu>
2020-09-02 21:53:19 -0600

    Counts each time a word is used, but counts capital words seperately. Nearing completion.

--- countWords.py
+++ countWords.py
@@ -9,15 +9,33 @@ import re
 #Pruitt/Freudenthal
 
 #initializing the input files and dictionary
-imputFile = sys.argv[1]
+inputFile = sys.argv[1]
 outputFile = sys.argv[2]
-dictionary = {}
+dictionary = dict()
 
 
 with open(inputFile,"r") as inFile:
     #iterating through the lines
     for line in inFile:
-        fileLine = re.split('\W',line)
 
+        
+        fileLine = re.split('\W',line)
+        #iterating through every word in the line
         for word in fileLine:
-            
+
+            #if the word is already in the dictionary, increment by one.
+            if word in dictionary:
+                dictionary[word] +=1
+                #If not in dictionary, add to dictionary
+            else:
+                dictionary[word] = 1
+
+
+with open(outputFile, "w") as outFile:
+    print("Outputting")
+    #Sorting the dictionary for descending order
+    for i in sorted(dictionary.keys()):
+        
+        print(dictionary[i])
+        outFile.write(i+" : "+str(dictionary[i])+"\n")
+    outFile.close()
